13623a1db5d7459f616fc50e421a7a2ca2ef3416
Luclu7 <luclu7@luclu7.fr>
2019-01-03 00:38:58 +0000

    dialog ~everywhere, auto return to the main menu

--- ui.sh
+++ ui.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 DIALOG=${DIALOG=dialog}
 SSHKEY=$1
+function case-dialog {
+case $choix in
+'Add')          add;;
+'Reset')        cp db.sqlite.clean db.sqlite && start_menu;; # yeah this won't be available for everyone
+'Remove')       remove;;
+'List')         list;;
+'Get')          get;;
+'Edit')         exit;;
+'About')        exit;;
+esac
+}
 
 function add {
 echo "\$SSHKEY is" $SSHKEY
 existantkey=$(sqlite3 db.sqlite "SELECT * FROM keys WHERE key=='$SSHKEY';")
 
 if [[ $existantkey != "" ]]; then
-echo "Your key is already in the database."
+dialog --title "SKS - Add your key" --msgbox "Your key is already in the database." 5 40
 else
-echo "What's your name?"
-read name
+name=$(dialog --title "SKS - Add your key" --inputbox "What is your name?" 8 40 3>&1 1>&2 2>&3 3>&-)
+echo $name
 echo "Adding you key to the database..."
 sqlite3 db.sqlite <<EOF
 INSERT INTO keys(pseudo,key) VALUES('$name','$SSHKEY');
-select * from keys;
 EOF
 fi
+start_menu
 } 
 
-function case-dialog {
-case $choix in
-'Add')		add;;
-'Reset')	cp db.sqlite.clean db.sqlite;; # yeah this won't be available for everyone
-'Remove')	remove;;
-'List')		list;;
-'Get')		get;;
-'Edit')		exit;;
-'About')	exit;;
-esac
-}
-
 function remove {
-echo removing your key
 sqlite3 db.sqlite <<EOF
 DELETE FROM keys WHERE key=='$SSHKEY';
 select * from keys;
 EOF
+dialog --title "SKS - Remove your key" --msgbox "Your key has been removed from thhe database." 6 40
+start_menu
 }
 
 function list {
-sqlite3 db.sqlite <<EOF
-select * from keys;
-EOF
+keys=$(sqlite3 db.sqlite "select * from keys;")
+dialog --title "SKS - List keys" --msgbox "$keys" 20 90
+start_menu
 }
 
 function get {
 echo "Whose key do you want?"
-read name
-sqlite3 db.sqlite <<EOF
-SELECT * FROM keys WHERE pseudo LIKE '$name';
-EOF
+name=$(dialog --title "SKS - Get specific user's keys" --inputbox "Whose key do you want??" 8 40 3>&1 1>&2 2>&3 3>&-)
+keys=$(sqlite3 db.sqlite "SELECT * FROM keys WHERE pseudo LIKE '$name';")
+dialog --title "SKS - Get specific user's keys" --msgbox "$keys" 20 90
+start_menu
 }
 
 #$DIALOG --title "SKS - Key" --clear \
@@ -61,6 +61,7 @@ EOF
 #	255)	exit 1;;
 #esac
 
+function start_menu {
 fichtemp=`tempfile 2>/dev/null` || fichtemp=/tmp/test$$
 trap "rm -f $fichtemp" 0 1 2 5 15
 $DIALOG --clear --title "SKS - Manage" \
@@ -80,3 +81,6 @@ case $valret in
 1)	echo "Appuyé sur Annuler.";;
 255)	echo "Appuyé sur Echap.";;
 esac
+}
+
+start_menu
